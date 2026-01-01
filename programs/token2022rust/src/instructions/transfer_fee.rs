8495bef6bfd3451d712ac91ac6b91834d8576a73
Codeindeed <favouralex084@mail.com>
2026-01-01 23:51:02 +0100

    mint structs

---
+++ programs/token2022rust/src/instructions/transfer_fee.rs
@@ -0,0 +1,20 @@
+use anchor_lang::system_program::{create_account, CreateAccount};
+use anchor_spl::{
+    token_2022::{
+        initialize_mint2,
+        spl_token_2022::{
+            extension::{
+                transfer_fee::TransferFeeConfig, BaseStateWithExtensions, ExtensionType,
+                StateWithExtensions,
+            },
+            pod::PodMint,
+            state::Mint as MintState,
+        },
+        InitializeMint2,
+    },
+    token_interface::{
+        spl_pod::optional_keys::OptionalNonZeroPubkey, transfer_fee_initialize, Token2022,
+        TransferFeeInitialize,
+    },
+};
+
--- programs/token2022rust/src/lib.rs
+++ programs/token2022rust/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{TokenInterface, Mint, TokenAccount};
+use anchor_spl::associated_token::AssociatedToken;
 
 declare_id!("28MPfdpbBHx1dbBFy1g7Qj2Rkz6rzxMdQ146ZkcdXGgM");
 
@@ -13,4 +15,45 @@ pub mod token2022rust {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateMint<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(init,payer = signer, mint::decimals = 6, mint::authority = signer.key(), mint::token_program = token_program)]
+    pub mint: Account<'info,Mint>,
+    pub system_program: Program<'info,System>,
+    pub token_program:Interface<'info,TokenInterface>,
+}
+
+#[derive(Accounts)]
+pub struct Createssociatedtokenaccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub mint: Interface<'info, Mint>,
+    #[account(
+        init,
+        payer = signer,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+        associated_token::token_program = token_program
+    )]
+    pub associated_token: InterfaceAccount<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct CreateToken<'info>{
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    pub mint: InterfaceAccount<'info,Mint>,
+    #[account(
+        mut,
+        token::mint = mint,
+        token::authority = signer,
+        token::token_program = token_program
+    )]
+    pub token: InterfaceAccount<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
